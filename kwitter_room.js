7bd41b6be3f1335225d5f356e6baaee47ec23ab5
JaiVerma24239 <130062417+JaiVerma24239@users.noreply.github.com>
2023-10-09 15:42:08 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCTf_zKldvTVAInT6kH3ziXR0ULSqZrhkE",
+    authDomain: "letschat-e72f5.firebaseapp.com",
+    projectId: "letschat-e72f5",
+    storageBucket: "letschat-e72f5.appspot.com",
+    messagingSenderId: "1057943400421",
+    appId: "1:1057943400421:web:10f724665737ad1f5fc90c"
+  };
+  
+  // Initialize Firebase
+  var app = initializeApp(firebaseConfig);
